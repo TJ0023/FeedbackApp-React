241c7368864cfdbb1a4847b2ea2f5220e9286ab7
TJ0023 <GimenezTJV@gmail.com>
2022-06-20 21:25:44 +0800

    Initial commit

---
+++ src/src/components/FeedbackItem.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Card from './shared/Card'
+import PropTypes from 'prop-types'
+import { FaTimes } from 'react-icons/fa'
+
+function FeedbackItem({item, handleDelete}) {
+
+  return (
+    <Card>
+        <div className="num-display">{item.rating}</div>
+        <button onClick={() => handleDelete(item.id)} className='close'>
+          <FaTimes color='purple '/>
+        </button>
+        <div className="text-display">{item.text}</div>
+    </Card>
+  )
+}
+
+
+FeedbackItem.propTypes = {
+    item: PropTypes.object.isRequired
+}
+export default FeedbackItem
